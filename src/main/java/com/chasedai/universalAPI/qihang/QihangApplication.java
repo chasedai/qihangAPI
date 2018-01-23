4618d303dc6d2cc4ee7d94908ab2f8d6e60a47b6
Cheehung <chasedai@icloud.com>
2018-01-23 19:38:20 +0000

    environment setup

---
+++ src/main/java/com/chasedai/universalAPI/qihang/QihangApplication.java
@@ -0,0 +1,12 @@
+package com.chasedai.universalAPI.qihang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QihangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QihangApplication.class, args);
+	}
+}
